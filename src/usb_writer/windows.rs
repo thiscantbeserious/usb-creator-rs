dae480b604f3bad40d41b9b901c0b7e9e0305ca3
Simon Sanladerer <s.sanladerer@ampeersenergy.de>
2024-03-10 13:00:02 +0100

    windows: add rough apis needed for drive discovery (windows api crate)

--- src/usb_writer/windows.rs
+++ src/usb_writer/windows.rs
@@ -1,5 +1,23 @@
 use super::*;
 
+// https://codentium.com/guides/windows-dev/windows-drivers-in-rust-io-controls/
+
+use ::windows::Win32::Storage::FileSystem::{
+    FindFirstVolumeW, 
+    FindNextVolumeW, 
+    GetVolumePathNamesForVolumeNameW,
+    CreateFileW, FILE_SHARE_READ, OPEN_EXISTING,
+};
+use ::windows::Win32::System::Ioctl::{
+    IOCTL_DISK_GET_DRIVE_GEOMETRY,
+    IOCTL_STORAGE_QUERY_PROPERTY, 
+    STORAGE_PROPERTY_QUERY
+};
+
+use ::windows::Win32::System::IO:: {
+    DeviceIoControl
+};
+
 pub struct WindowsUsbWriter;
 
 impl UsbWriter for WindowsUsbWriter {
