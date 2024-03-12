e7e3c989f1b92d6775f5b3c8ab5ce8ac4da528cd
Simon Sanladerer <s.sanladerer@ampeersenergy.de>
2024-03-12 12:09:38 +0100

    linux: add example json for mocking prep

--- src/usb_writer/macos.rs
+++ src/usb_writer/macos.rs
@@ -10,6 +10,7 @@ use plist::Value;
 pub struct MacOSUsbWriter;
 
 // TODO example plist for Mocking
+// $diskutil list -plist external
 // <?xml version="1.0" encoding="UTF-8"?>
 // <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 // <plist version="1.0">
